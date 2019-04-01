6acee9eb2852260173c70b116b83b881c1414152
Dominic <dvphan@ucdavis.edu>
2019-04-01 15:37:27 -0700

    default message towards mention

--- controllers/Bot/EventConsumer.ts
+++ controllers/Bot/EventConsumer.ts
@@ -1,10 +1,12 @@
+import axios, { AxiosResponse } from 'axios';
 import EventEmitter = require('events');
+import nconf = require('nconf');
 import { queueName } from '../../utils/redisConfig';
 import { MEMBER_JOINED_CHANNEL, APP_MENTION } from './EventProducer';
 import RedisSMQ = require('rsmq');
 
-
 export class EventConsumer extends EventEmitter {
+    private webhookURL: string;
     private timeout: number;
     private rsmq;
     constructor(timeout = 100) {
@@ -14,6 +16,8 @@ export class EventConsumer extends EventEmitter {
             host: "redis-mq",
             ns: "rsmq"
         });
+        nconf.argv().env().file('keys.json');
+        this.webhookURL = nconf.get('webhook');
     }
 
     public checkMsgQueue = () => {
@@ -29,6 +33,13 @@ export class EventConsumer extends EventEmitter {
             .then((resp) => {
                 if (resp.id) {
                     console.log("message received", resp);
+                    const message = JSON.parse(resp.message);
+                    switch (message.event.type) {
+                        // case APP_MENTION:
+                        // case MEMBER_JOINED_CHANNEL:
+                        default:
+                            this.unknownCommand(resp.id);
+                    }
                 } else {
                     console.log("no message here");
                 }
@@ -42,8 +53,20 @@ export class EventConsumer extends EventEmitter {
 
     }
 
-    private unknownCommand = async () => {
-
+    private unknownCommand = async (messageID) => {
+        let response: AxiosResponse = await axios.post(this.webhookURL, { text: 'Hi, I don\'t understand your command' });
+        if (response.status >= 200 && response.status < 400) {
+            this.rsmq.deleteMessage({
+                qname: queueName,
+                id: messageID
+            }, (err, resp) => {
+                if (resp === 1) {
+                    console.log('message deleted');
+                } else {
+                    console.log('message not found');
+                }
+            })
+        }
     }
 }
 
